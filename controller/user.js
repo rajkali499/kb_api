a8687afb0acfc10b7ae666d525c7f66e62c254ce
kalirajan <rajkali499@gmail.com>
2022-10-17 14:59:12 +0530

    change of user update for existing user and new

--- controller/user.js
+++ controller/user.js
@@ -11,7 +11,7 @@ module.exports.getAllUser = (req, res) => {
 			id: sort,
 		})
 		.then((users) => {
-			res.json({users:users});
+			res.json({ users: users });
 		})
 		.catch((err) => console.log(err));
 };
@@ -24,84 +24,112 @@ module.exports.getUser = (req, res) => {
 	})
 		.select(['-_id'])
 		.then((user) => {
-			res.json({user:user});
+			res.json({ user: user });
 		})
 		.catch((err) => console.log(err));
 };
 
-module.exports.addUser = (req, res) => {
+  module.exports.addUser =  (req, res)  => {
 	if (typeof req.body == undefined) {
 		res.json({
 			status: 'error',
 			message: 'data is undefined',
 		});
 	} else {
-		let userCount = 0;
-		User.find()
-			.countDocuments(function (err, count) {
-				userCount = count;
-			})
-			.then(() => {
-				const user = new User({
-					id: req.body.id,
-					email: req.body.email,
-					username: req.body.username,
-					password: req.body.password,
-					name: {
-						firstname: req.body.name.firstname,
-						lastname: req.body.name.lastname,
-					},
-					address: {
-						city: req.body.address.city,
-						street: req.body.address.street,
-						doornumber: req.body.address.doornumber,
-						zipcode: req.body.address.zipcode,
-						geolocation: {
-							lat: req.body.address.geolocation.lat,
-							long: req.body.address.geolocation.long,
+		let user1 = new User();
+		 User.findOne({ id: req.body.id })
+			.then((user) => {
+				user1 = user;
+		
+		if (user1 != null) {
+			user1.id = req.body.id;
+			user1.email = req.body.email;
+			user1.username = req.body.username;
+			user1.password = req.body.password;
+			user1.name.firstname = req.body.firstname;
+			user1.address.city = req.body.address.city;
+			user1.address.street = req.body.address.street;
+			user1.address.doornumber = req.body.address.doornumber;
+			user1.address.zipcode = req.body.address.zipcode;
+			user1.address.geolocation.lat = req.body.address.geolocation.lat;
+			user1.address.geolocation.long = req.body.address.geolocation.long;
+			user1.phone = req.body.phone;
+			
+			user1.save()
+				.then(user => res.json({ user: user }))
+				.catch(err => console.log(err))
+		}
+		else {
+			let userCount = 0;
+			User.find()
+				.countDocuments(function (err, count) {
+					userCount = count;
+				})
+				.then(() => {
+					const user = new User({
+						id: req.body.id,
+						email: req.body.email,
+						username: req.body.username,
+						password: req.body.password,
+						name: {
+							firstname: req.body.name.firstname,
+							lastname: req.body.name.lastname,
 						},
-					},
-					phone: req.body.phone,
-				});
-				user.save()
-				  .then(user => res.json({user:user}))
-				  .catch(err => console.log(err))
+						address: {
+							city: req.body.address.city,
+							street: req.body.address.street,
+							doornumber: req.body.address.doornumber,
+							zipcode: req.body.address.zipcode,
+							geolocation: {
+								lat: req.body.address.geolocation.lat,
+								long: req.body.address.geolocation.long,
+							},
+						},
+						phone: req.body.phone,
+					});
+					user.save()
+						.then(user => res.json({ user: user }))
+						.catch(err => console.log(err))
 
-				// res.json(user);
-			});
+					// res.json(user);
+				})}
+			})
+			.catch((e) =>console.log(e));
 
-		//res.json({id:User.find().count()+1,...req.body})
-	}
-};
+			//res.json({id:User.find().count()+1,...req.body})
+		}
+	};
 
-module.exports.editUser = (req, res) => {
+module.exports.editUser =  (req, res) => {
 	if (typeof req.body == undefined || req.params.id == null) {
 		res.json({
 			status: 'error',
 			message: 'something went wrong! check your sent data',
 		});
 	} else {
-		res.json({user:{
-			id: parseInt(req.params.id),
-			email: req.body.email,
-			username: req.body.username,
-			password: req.body.password,
-			name: {
-				firstname: req.body.name.firstname,
-				lastname: req.body.name.lastname,
-			},
-			address: {
-				city: req.body.address.city,
-				street: req.body.address.street,
-				doornumber: req.body.address.doornumber,
-				zipcode: req.body.address.zipcode,
-				geolocation: {
-					lat: req.body.address.geolocation.lat,
-					long: req.body.address.geolocation.long,
+		res.json({
+			user: {
+				id: parseInt(req.params.id),
+				email: req.body.email,
+				username: req.body.username,
+				password: req.body.password,
+				name: {
+					firstname: req.body.name.firstname,
+					lastname: req.body.name.lastname,
 				},
-			},
-			phone: req.body.phone,
-		}});
+				address: {
+					city: req.body.address.city,
+					street: req.body.address.street,
+					doornumber: req.body.address.doornumber,
+					zipcode: req.body.address.zipcode,
+					geolocation: {
+						lat: req.body.address.geolocation.lat,
+						long: req.body.address.geolocation.long,
+					},
+				},
+				phone: req.body.phone,
+			}
+		});
 	}
 };
 
@@ -115,7 +143,7 @@ module.exports.deleteUser = (req, res) => {
 		User.findOne({ id: req.params.id })
 			.select(['-_id'])
 			.then((user) => {
-				res.json({user:user});
+				res.json({ user: user });
 			})
 			.catch((err) => console.log(err));
 	}
